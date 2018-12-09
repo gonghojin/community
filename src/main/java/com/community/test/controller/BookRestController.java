8ba0718d809ec387cff7e0783d701a64dfb5b83c
gonghojin <yolip51@naver.com>
2018-12-09 23:21:20 +0900

    Add : RestAPI, JSON Test[p 85 ~ 92]

---
+++ src/main/java/com/community/test/controller/BookRestController.java
@@ -0,0 +1,19 @@
+package com.community.test.controller;
+
+import com.community.test.domain.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookRestController {
+
+    @Autowired
+    BookRestService bookRestService;
+
+    @GetMapping(path = "/rest/test", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Book getRestBooks() {
+        return bookRestService.getRestBook();
+    }
+}
--- src/test/java/com/community/pojo/PropertyTest.java
+++ src/test/java/com/community/pojo/PropertyTest.java
@@ -7,7 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
-import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
